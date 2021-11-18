7bc80495d98b84ceeeb620b7528e2e041bcf4dae
CedricWillemsEHB <cedric.willems@student.ehb.be>
2021-11-18 09:42:54 +0100

    first push

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from 'src/events/events-list.component';
+import { EventThumbnailComponent } from 'src/events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
+
+@NgModule({
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
---
+++ src/events/event-thumbnail.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+
+@Component({
+    selector: 'event-thumbnail',
+    template:`
+    <div class="well hoverwell thumbnail">
+        <h2>{{event.name}}</h2>
+        <div>Date: {{event.date}}</div>
+        <div>Time: {{event.time}}</div>
+        <div>Price: \€{{event.price}}</div>
+        <div>
+            <span>Location: {{event.location.address}}</span>
+            <span>&nbsp;</span>
+            <span>{{event.location.city}}, {{event.location.country}}</span>
+        </div>
+    </div>
+    `,
+    styles: [`
+        .thumbnail { min-height: 210px; }
+        .pad-left { margin-left: 10px; }
+        .well div { color: #aaa; }
+    `]
+})
+export class EventThumbnailComponent {
+    @Input() event:any
+}
